9d2f6f8fbff4fb89a73f81b3c4028c7a6321534f
skybigstar <skybigstar1990@gmail.com>
2023-12-08 13:10:36 +0900

    third

--- backend/main.py
+++ backend/main.py
@@ -2,14 +2,14 @@ from flask import Flask, request
 from flask_cors import CORS
 from openai import OpenAI
 import os
-import numpy as np
+import json
 
 
 app = Flask(__name__)
 CORS(app)
 
 
-openAI_key = "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"
+openAI_key = "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"
 
 
 # def generate_text(openAI_key, prompt, engine="text-davinci-003"):
@@ -27,27 +27,49 @@ openAI_key = "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"
 #     return message
 
 
-def generate_text2(openAI_key, prompt, engine="gpt-3.5-turbo"):
+def generate_text2(openAI_key, prompt, engine="gpt-4"):
 
     client = OpenAI(api_key=openAI_key)
+    system_prompt  = f"""
+You are a very friendly and beautiful girl who users should chat with.
+here, your hobby is football and swimming.
+Your name is Arina and height is 170cm.
+You live in USA and you was born in USA.
 
+Input indicator:
+
+    
+Output indicator:
+   - The output should be ask reason when the other is saying about his feeling.
+   - The output must not be a robot or OpenAI.
+      Must be like human's
+      Must not contain phrases involving openai or AI models.
+   - The output should be simple.
+
+Note:
+   - The output should be human-like.
+"""
+    print(prompt)
     messages = [
-        {"role": "system", "content": "You are a helpful chatbot about sports"},
+        {"role": 'system', "content": system_prompt},
         {"role": "user", "content": prompt},
     ]
 
     response = client.chat.completions.create(
-        model="gpt-3.5-turbo",
+        model=engine,
         messages=messages,
         stream=True,
     )
-    print(response.choices[0].message.content)
-    # return message
+    message =""
+    for chunk in response:
+        if chunk.choices[0].delta.content is not None:
+            message += chunk.choices[0].delta.content
+    return message
 
 
 def generate_answer(question, openAI_key):
 
-    full_question = f"Please answer the questions which is related to sports. If the question is not related to sports, then answer I don't know"
+    full_question = question
     answer = generate_text2(openAI_key, full_question)
     return answer
 
@@ -56,7 +78,11 @@ def generate_answer(question, openAI_key):
 def context():
     question = request.json["query"]
     result = generate_answer(question, openAI_key)
-    return result
+    data = {
+        "type":"generic",
+        "content":result
+    }
+    return json.dumps(data)
 
 
 if __name__ == "__main__":
